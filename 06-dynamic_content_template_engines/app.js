aa695a5923d4a8123063978d9af0ab00781598c2
Antonio Merino <antoniomerino91@gmail.com>
2025-02-24 14:02:01 +0100

    Finalizada sección 6 - Parte de Pug

---
+++ 06-dynamic_content_template_engines/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+//En el browser /favicon.ico también genera una request, que tenemos que tratar correctamente para que sea ignorada
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page not foundooooo'});
+});
+
+app.listen(3000);
